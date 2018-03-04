231a81f3e70e1e0dc9d89b4b64a36f0ce5bb18c0
ca-montenegro <ca.montenegro@uniandes.edu.co>
2018-03-04 10:49:46 -0500

    Navbar working. New components: about, contact, home

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
