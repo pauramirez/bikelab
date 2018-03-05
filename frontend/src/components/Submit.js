c852f13bca8b6e1a7deff316bf9e412225d63344
CarlosP97 <c.penaloza@uniandes.edu.co>
2018-03-04 20:03:28 -0500

    Starting accessory post

--- frontend/src/components/Submit.js
+++ frontend/src/components/Submit.js
@@ -18,54 +18,57 @@ class Submit extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
-                                <div className="row">
-                                    <div className="col-lg-6">
-                                        <div className="form-group">
-                                            <label>Accessory name</label>
-                                            <input name="name" className="form-control" type="text"/>
+
+                                <form onSubmit={this.addAccessory.bind(this)}>
+                                    <div className="row">
+                                        <div className="col-lg-6">
+                                            <div className="form-group">
+                                                <label>Accessory name</label>
+                                                <input name="name" className="form-control" type="text"/>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <div className="form-group">
+                                                <label>Brand</label>
+                                                <input name="name" className="form-control" type="text"/>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <div className="form-group">
+                                                <label> Category</label>
+                                                <select className="form-control js-search-category" name="category" data-placeholder="Choose Category" aria-hidden="true">
+                                                    <option value="Other">Other</option>
+                                                    <option value="Bike Locks">Bike Locks</option>
+                                                    <option value="Bike Pumps">Bike Pumps</option>
+                                                    <option value="Bike Mirrors">Bike Mirrors</option>
+                                                    <option value="Bike Covers">Bike Covers</option>
+                                                    <option value="Bike Bells">Bike Bells</option>
+                                                    <option value="Lights & Reflectors">Lights & Reflectors</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <div className="form-group">
+                                                <label>Upload your photos</label>
+                                                <input className="form-control-file" type="file"/>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="col-lg-6">
-                                        <div className="form-group">
-                                            <label>Brand</label>
-                                            <input name="name" className="form-control" type="text"/>
-                                        </div>
+
+                                    <div className="form-group">
+                                        <label>Accessory price</label>
+                                        <input name="price" className="form-control" type="number"/>
                                     </div>
-                                    <div className="col-lg-6">
-                                        <div className="form-group">
-                                            <label> Category</label>
-                                            <select className="form-control js-search-category" name="category" data-placeholder="Choose Category" aria-hidden="true">
-                                                <option value="1">Other</option>
-                                                <option value="2">Bike Locks</option>
-                                                <option value="3">Bike Pumps</option>
-                                                <option value="4">Bike Mirrors</option>
-                                                <option value="5">Bike Covers</option>
-                                                <option value="6">Bike Bells</option>
-                                                <option value="7">Lights & Reflectors</option>
-                                            </select>
-                                        </div>
+
+
+
+                                    <div className="form-group">
+                                        <label>Description</label>
+                                        <textarea className="form-control" rows="4" required="required"></textarea>
                                     </div>
-                                    <div className="col-lg-6">
-                                        <div className="form-group">
-                                            <label>Upload your photos</label>
-                                            <input className="form-control-file" type="file"/>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div className="form-group">
-                                    <label>Accessory price</label>
-                                    <input name="price" className="form-control" type="number"/>
-                                </div>
-
-
-
-                                <div className="form-group">
-                                    <label>Description</label>
-                                    <textarea className="form-control" rows="4" required="required"></textarea>
-                                </div>
-
-                                <button type="submit" className="btn btn-submit">Submit Accessory</button>
+                                    <button type="submit" className="btn btn-submit">Submit Accessory</button>
+                                </form>
 
                             </div>
                         </div>
